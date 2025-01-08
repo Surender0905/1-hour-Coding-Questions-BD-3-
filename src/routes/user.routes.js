5bba4c11a65286440677f299f2d4651154ee98da
Surender Pratap Singh <rk31151@gmail.com>
2025-01-08 15:22:55 +0530

    bug fixing code

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAllByUser } from "../controllers/user.controller";
+import { getAllByUser } from "../controllers/user.controller.js";
 
 const router = Router();
 
